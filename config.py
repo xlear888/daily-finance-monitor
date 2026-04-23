321f819799247621500dbb4649cb22223b8dc16a
xlear888 <franklu2008@gmail.com>
2026-04-23 16:38:28 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+WATCHLIST = ["2330", "2454", "2317", "2882"]
+US_WATCH_SYMBOLS = ["TSM", "AAPL", "NVDA"]
+
+COMPANY_NAMES = {
+    "2330": ["台積電", "TSMC"],
+    "2454": ["聯發科", "MediaTek"],
+    "2317": ["鴻海", "Foxconn"],
+    "2882": ["國泰金", "Cathay"]
+}
+
+DAYS_BACK = 2
